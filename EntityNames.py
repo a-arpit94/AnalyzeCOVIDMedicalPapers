9dbb8fe33a20e9f031b363d8d3df6fa7a9cd36ba
Arpit Agarwal <a.arpit94@gmail.com>
2022-04-16 06:14:06 +0000

    update EntityNames.py

--- EntityNames.py
+++ EntityNames.py
@@ -1,11 +1,11 @@
 class EntityNames:
 
-    SUBSCRIPTION_ID = '153fcf1d-a6e8-4fde-b526-5b7bd819d79d'
+    SUBSCRIPTION_ID = 'xxxx'
     
     azureCognitiveServiceName = "analyzecovidmedicalpaper-cg"
     
     AZURE_COGNITIVE_SERVICES_API_KEY = {
-        azureCognitiveServiceName:  '7ff92721d161485fbec2c404ad5ce770'
+        azureCognitiveServiceName:  'xxxx'
         }
 
     resourcegroup = 'analyzecovidmedicalpaper-rg'
@@ -17,7 +17,7 @@ class EntityNames:
     cosmosDBAccountName = 'analyzecovidmedpaper-db'
     
     AZURE_COSMOSDB_KEY = {
-        cosmosDBAccountName:  'zaWgSQoJUvCVOSm4YL1jmm9Lbuq3Ww2wsDEaXJ8CNbM7T3tlihHvjghrskEl3zJKfq8dScdLi1K7MM5dMnYbxw=='
+        cosmosDBAccountName:  'xxxx'
         }
 
     medPaperDBName = 'CORD'
