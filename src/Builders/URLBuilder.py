893a5ad13ca763fbd928d36ba41d7ab89677eca6
Arpit Agarwal <a.arpit94@gmail.com>
2022-04-16 05:25:53 +0000

    remove src folder

--- src/Builders/URLBuilder.py
+++
@@ -1,9 +0,0 @@
-class URLBuilder:
-
-    @staticmethod
-    def buildCognitiveServicesEndpoints(serviceName):
-        return f"https://{serviceName}.cognitiveservices.azure.com/"
-
-    @staticmethod
-    def buildCosmosDBEndpoints(databaseName):
-        return f"https://{databaseName}.documents.azure.com:443/"
