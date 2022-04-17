99b648bc9f08666e8cb1e19266e05bdbed917cd3
Arpit Agarwal <a.arpit94@gmail.com>
2022-04-17 00:37:04 +0000

    update main.py file

--- main.py
+++ main.py
@@ -4,39 +4,49 @@ from Providers.CosmosDBProvider import CosmosDBProvider
 from Models.HealthcareRelation import HealthcareRelation
 from Models.HealthcareEntity import HealthcareEntity
 from Models.AnalyzeHealthcareEntitiesResult import AnalyzeHealthcareEntitiesResult
+import sys
 
 def main():
     MEDICAL_PAPER_COUNT = 1
-    dataset = MachineLearningProvider().getMLDataset(numberOfRows = 10)
+    dataset = MachineLearningProvider().getMLDataset(numberOfRows = 1000)
     
     # listOfMedPaperAbstracts = dataset['abstract'].astype(str).values.tolist()
     db = CosmosDBProvider()
 
-    CHUNK_SIZE = 2
+    CHUNK_SIZE = 5
 
-    for i in range(0, len(dataset), CHUNK_SIZE):
-        temp_abstract_list = [dataset.loc[i+a, 'abstract'] for a in range(CHUNK_SIZE)]
-        
-        poller =  TextAnalyticsProvider().getHealthcareEntities(document = temp_abstract_list)
-        textAnalysisResult = list(poller.result())
-        
-        for idx, item in enumerate(textAnalysisResult):
-            entities = []
-            entity_relations = []
+    try:
+        for i in range(0, len(dataset), CHUNK_SIZE):
+            try:
+                temp_abstract_list = []
+                temp_abstract_list = [dataset.loc[i+a, 'abstract'] for a in range(CHUNK_SIZE)]
+                poller =  TextAnalyticsProvider().getHealthcareEntities(document = temp_abstract_list)
+                textAnalysisResult = list(poller.result())
+                
+                for idx, item in enumerate(textAnalysisResult):
+                    entities = []
+                    entity_relations = []
 
-            for entity in item.entities:
-                healthcareEntity = HealthcareEntity(entity)
-                entities.append(healthcareEntity.to_dict())
+                    for entity in item.entities:
+                        healthcareEntity = HealthcareEntity(entity)
+                        entities.append(healthcareEntity.to_dict())
 
-            for relation in item.entity_relations:
-                healthcareRelation = HealthcareRelation(relation)
-                entity_relations.append(healthcareRelation.to_dict())
-            
-            analyzeHealthcareEntitiesResult = AnalyzeHealthcareEntitiesResult(dataset.loc[i+idx], entities, entity_relations)
-            db.upsertDataToContainer(analyzeHealthcareEntitiesResult.to_dict())
-            
-            print("Medical Paper Processed:", MEDICAL_PAPER_COUNT)
-            MEDICAL_PAPER_COUNT+=1
+                    for relation in item.entity_relations:
+                        healthcareRelation = HealthcareRelation(relation)
+                        entity_relations.append(healthcareRelation.to_dict())
+                    
+                    analyzeHealthcareEntitiesResult = AnalyzeHealthcareEntitiesResult(dataset.loc[i+idx], entities, entity_relations)
+                    db.upsertDataToContainer(analyzeHealthcareEntitiesResult.to_dict())
+                    
+                    print("Medical Paper Processed:", MEDICAL_PAPER_COUNT)
+                    MEDICAL_PAPER_COUNT+=1
+                
+            except:
+                print(f"ERROR OCCURRED FOR PAPER NUMBER {MEDICAL_PAPER_COUNT} :: {sys.exc_info()[0]}")
+                MEDICAL_PAPER_COUNT+=1
+
+    except:
+        pass
 
 
 if __name__ == "__main__":
