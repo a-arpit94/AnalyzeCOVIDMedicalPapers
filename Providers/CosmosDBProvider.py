729924dc58dc5cc5e795b6d1912ed477d541451c
Arpit Agarwal <a.arpit94@gmail.com>
2022-04-22 18:55:12 +0000

    update cosmos container name

--- Providers/CosmosDBProvider.py
+++ Providers/CosmosDBProvider.py
@@ -8,7 +8,7 @@ class CosmosDBProvider:
     def __init__(self) -> None:
         cosmosClient = CosmosDBService(EntityNames.cosmosDBAccountName)
         cosmosClient.setDatabase(EntityNames.medPaperDBName)
-        self.containerClient = cosmosClient.getDatabaseContainerClient( EntityNames.testDBContainer)
+        self.containerClient = cosmosClient.getDatabaseContainerClient( EntityNames.medPaperContainer)
 
     def upsertDataToContainer(self, data):
         self.containerClient.upsert_item(data)
